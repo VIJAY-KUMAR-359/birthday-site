5f7e52ce255c7385d7d24528f561e9923a1e007e
VIJAY-KUMAR-359 <vijaykumareppili359@gmail.com>
2026-03-18 18:41:01 +0530

    updated changes

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -14,7 +14,7 @@ export default function Home() {
   const [isLoading, setIsLoading] = useState(true)
   const [bubbles, setBubbles] = useState([])
   const [showForYouBtn, setShowForYouBtn] = useState(false)
-  const birthdayDate = new Date("March 19, 2026") // Change this date accordingly
+  const birthdayDate = new Date("March 1, 2026") // Change this date accordingly
   const audioRef = useRef(null)
 
   // For testing
