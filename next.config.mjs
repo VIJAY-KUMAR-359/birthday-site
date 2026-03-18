50c1331df412428914944c97596af84effe62a8a
VIJAY-KUMAR-359 <vijaykumareppili359@gmail.com>
2026-03-18 17:01:00 +0530

    final fix white screen

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
-
+    basePath: "/birthday-site",
+    assetPrefix: "/birthday-site/",
 };
 
 export default nextConfig;
